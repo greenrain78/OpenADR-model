a83cd2e9c0bc0a5df80ef5812f4399326e892b29
daewon kim <kimdw010130@daum.net>
2021-11-28 22:08:44 +0900

    sw 녹화

---
+++ model/sample_model2.py
@@ -0,0 +1,14 @@
+from Controller.engine_class import Engine
+
+print("hello sample py")
+
+
+def hello_def():
+    print("hello_def")
+
+
+class hello_class(Engine):
+    pass
+
+    def container_run(self):
+        print("sample model 2 sw 심화 프로잭트 녹화")
